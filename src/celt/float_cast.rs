1560b347169922bfd67db50f66ac051103b6055d
⭐️NINIKA⭐️ <dcnick3@users.noreply.github.com>
2024-06-21 20:10:33 +0300

    fix: incorrect rounding used in `float2int`
    Also put the clapping back into `FLOAT2INT16`

--- src/celt/float_cast.rs
+++ src/celt/float_cast.rs
@@ -2,9 +2,9 @@ pub const CELT_SIG_SCALE: f32 = 32768.0f32;
 
 #[inline]
 pub fn FLOAT2INT16(x: f32) -> i16 {
-    (x * CELT_SIG_SCALE) as i16
+    (x * CELT_SIG_SCALE).max(-32768.0).min(32767.0) as i16
 }
 #[inline]
 pub fn float2int(x: f32) -> i32 {
-    x as i32
+    (x + 0.5).floor() as i32
 }
